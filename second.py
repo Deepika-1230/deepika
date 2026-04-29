d36810cf6c44732bb7bb02ee1e29356e03e5abaa
Deepika-1230 <ranadeepika200628@gmail.com>
2026-04-29 13:03:55 +0545

    first fille pushed

---
+++ second.py
@@ -0,0 +1,29 @@
+a=[1,2,3]
+b=[1,2,3]
+c=a
+print(a is b)
+print(a==b)
+print(a is c)
+print(id(a))
+print(id(b))
+print(id(c))
+print(6 and 3)
+print(5 or 6)
+print(0 and 0 )
+print(0 or 0)
+print(1 and 0)
+print(1 or 0)
+print("" or 0)
+print("" and 0)
+
+course={1,2,3,4,4}
+print(course)
+student_enrolled={
+    "name":"Deepika",
+    "age":19,
+"course"="cybersecurity",
+"subject"=["maths","science","english"]
+}
+print(student_enrolled)
+print(student_enrolled.key())
+print(student_enrolled["name"])
---
+++ third.py
@@ -0,0 +1,2 @@
+name ="softwarica"
+print(name)
